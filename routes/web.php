9adf66e65e4512f674ee602ee1c89670a3fecdc7
unknown <soumadeepc@gmail.com>
2026-01-14 17:59:50 +0000

    Lead upload routes added

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LeadImportController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/leads/upload', [LeadImportController::class, 'create'])->name('leads.upload.form');
+    Route::post('/leads/upload', [LeadImportController::class, 'store'])->name('leads.upload');
 });
